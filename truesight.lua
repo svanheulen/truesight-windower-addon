f66b903abe7618e1a416522e8adecc370a09a6a8
svanheulen <svanheulen@gmail.com>
2014-10-19 15:16:02 -0400

    block packet that makes killed monsters disappear

--- truesight.lua
+++ truesight.lua
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 _addon.name = 'truesight'
-_addon.version = '1.0.0'
+_addon.version = '1.0.1'
 _addon.command = 'truesight'
 _addon.author = 'Seth VanHeulen (Acacia@Odin)'
 
@@ -43,6 +43,8 @@ end
 function check_incoming_chunk(id, original, modified, injected, blocked)
     if id == 0x0E then
         return original:clearbits(257, 2)
+    elseif id == 0x38 and original:sub(13, 16) == 'kesu' then
+        return true
     end
 end
 
